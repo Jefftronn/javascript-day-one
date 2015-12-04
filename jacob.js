907962ed1b922610120efa4e11e9ef25dece5c43
Jacob Israel Turner <j.israel.turner@gmail.com>
2015-12-04 12:22:40 -0700

    initial commit

---
+++ jacob.js
@@ -0,0 +1,10 @@
+var me = {
+  name: 'Jacob Turner',
+  age: 22,
+  socialMedia: {
+    twitter: 'jisraelturner',
+    linkedin: 'https://www.linkedin.com/in/jacob-turner-620572a1',
+    github: 'jacob-israel-turner',
+    email: 'j.israel.turner@gmail.com'
+  }
+}
---
+++ resources.js
@@ -0,0 +1,4 @@
+var resources = {
+  evolutionOfTheWeb: 'http://www.evolutionoftheweb.com/?hl=en-gb',
+  webAppLandscape: 'http://alpha.ideavis.co/529cc5f/?utm_content=devmtnd15ac&utm_source=devmtn&utm_medium=devmtn&utm_campaign=devmtn'
+}
